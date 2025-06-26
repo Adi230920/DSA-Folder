db433340ef869742c4d54d9f175a071aeaccdedd
Aditya Mhatre <keepcallingaditya@gmail.com>
2025-06-26 17:20:34 +0530

    26 June commit

---
+++ Lec_07.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Lec_07 {
+
+    // Print a given name in a function 
+    public static void printMyName(String name) {
+        System.out.println(name);
+        return;
+    }
+
+    public static void main(String[] args) {
+
+        // Functions and Methods
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+        String name = sc.nextLine();
+
+        printMyName(name);
+        
+
+
+
+    }
+
+}
