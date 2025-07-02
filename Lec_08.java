f6c3dc0bd2d6d77b7809481ffbb51930b461ebe7
Aditya Mhatre <keepcallingaditya@gmail.com>
2025-07-02 13:11:43 +0530

    2 July Commit

--- Lec_08.java
+++ Lec_08.java
@@ -2,30 +2,53 @@ import java.util.*;
 
 public class Lec_08 {
 
+    /*
+     * Enter 3 numbers from the user & make a function to print their average.
+       Write a function to print the sum of all odd numbers from 1 to n.
+       Write a function which takes in 2 numbers and returns the greater of those two.
+       Write a function that takes in the radius as input and returns the circumference of a circle.
+       Write a function that takes in age as input and returns if that person is eligible to vote or not. A person of age > 18 is eligible to vote.
+       Write an infinite loop using do while condition.
+       Write a program to enter the numbers till the user wants and at the end it should display the count of positive, negative and zeros entered. 
+       Two numbers are entered by the user, x and n. Write a function to find the value of one number raised to the power of another i.e. xn.
+       Write a function that calculates the Greatest Common Divisor of 2 numbers. (BONUS)
+       Write a program to print Fibonacci series of n terms where n is input by user :
+       0 1 1 2 3 5 8 13 21 ..... 
+       In the Fibonacci series, a number is the sum of the previous 2 numbers that came before it.
+       (BONUS)
+     */
+
+
     // Function to calculate average of 3 float numbers
     public static float printAverage(float a, float b, float c) {
         float average = (a + b + c) / 3;
         return average;
     }
 
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the first number: ");
-        float a = sc.nextFloat();
+        //--------------------------------------------------------------------------------------
 
-        System.out.println("Enter the second number: ");
-        float b = sc.nextFloat();
+        // System.out.println("Enter the first number: ");
+        // float a = sc.nextFloat();
 
-        System.out.println("Enter the third number: ");
-        float c = sc.nextFloat();
+        // System.out.println("Enter the second number: ");
+        // float b = sc.nextFloat();
 
-        float avg = printAverage(a, b, c);
+        // System.out.println("Enter the third number: ");
+        // float c = sc.nextFloat();
 
-        // Print average with 2 decimal places
-        System.out.printf("The average of 3 numbers entered is %.2f\n", avg);
+        // float avg = printAverage(a, b, c);
 
-        sc.close(); // good practice to close Scanner
+        // System.out.printf("The average of 3 numbers entered is %.2f\n", avg);
+
+        //--------------------------------------------------------------------------------------
+
+        
+
+        
     }
 }
---
+++ Lec_10.java
@@ -0,0 +1,102 @@
+import java.util.*;
+
+public class Lec_10 {
+  public static void main(String[] args) {
+
+    // Arrays - Introduction
+
+    // Enter marks inside array and print them
+
+    // int[] marks = new int[4] ;
+    // //int marks [] = new int[4] ; --> This syntax is also correct for initiating
+    // an array
+    // marks[0] = 97 ; // physics
+    // marks[1] = 98 ; //chemistry
+    // marks[2] = 95 ; //english
+    // marks[3] = 92 ; //maths
+
+    // System.out.println(marks[0]);
+    // System.out.println(marks[1]);
+    // System.out.println(marks[2]);
+
+    // ----------------------------------------------------------------------------
+
+    // way 2 of defining an array
+
+    // int marks [] = {97 , 98 , 95 };
+
+    // for(int i=0 ; i<=marks.length; i++){
+    // System.out.println(marks[i]);
+    // }
+
+    // ---------------------------------------------------------------------------
+
+    // Scanner sc = new Scanner(System.in);
+
+    // // Take array size as input
+    // System.out.print("Enter the array size : ");
+    // int size = sc.nextInt();
+
+    // // Make an array of numbers
+    // int numbers[] = new int[size];
+
+    // // Take input values for array
+    // for (int i = 0; i < size; i++) {
+    //   System.out.print("Enter value to put in array : ");
+    //   numbers[i] = sc.nextInt();
+    // }
+
+    // System.out.println("Array elements are : ");
+    // // Print output of array values
+    // for (int i = 0; i < size; i++) {
+    //   System.out.println(numbers[i]);
+    // }
+
+    //------------------------------------------------------------------------
+
+    Scanner sc = new Scanner(System.in);
+
+    // Take array size as input 
+    System.out.print("Enter the array size : ");
+    int size = sc.nextInt();
+
+    int numbers[] = new int[size] ; 
+
+    //Take array input values 
+    for(int i=0 ; i<size;i++){
+      System.out.print("Enter the number to put in array : ");
+      numbers[i] = sc.nextInt();
+    }
+
+    System.out.println();
+
+    System.out.println("Array elements are : ");
+    for(int i=0 ; i<numbers.length ; i++){
+      System.out.print(numbers[i] + " ");
+    }
+
+    System.out.println();
+    System.out.println();
+
+    System.out.print("Enter the number to be searched : ");
+    int search_num = sc.nextInt();
+
+    System.out.println("Number Searching is : " + search_num);
+
+    for(int i=0 ; i<numbers.length ; i++){
+      if(numbers[i] == search_num){
+        System.out.println("Number Found at Index : " + i);
+      }
+    }
+
+    
+
+
+    
+
+
+
+
+  }
+
+}
