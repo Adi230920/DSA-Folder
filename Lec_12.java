45f689522b54034a2486bb764f2d821b14204ae9
Aditya Mhatre <keepcallingaditya@gmail.com>
2025-07-02 21:21:34 +0530

    2 July Commit

--- Lec_12.java
+++ Lec_12.java
@@ -29,16 +29,50 @@ public class Lec_12 {
         String lastName = "Stark";
         String fullName = firstName + " " + lastName ;
         System.out.println(fullName);
+
+        //------------------------------------------------------------------------------------
         
         // Length
         System.out.println(fullName.length());
+
+        //-------------------------------------------------------------------------------------
         
         //charAt
         for(int i=0 ; i<fullName.length() ; i++){
             System.out.println(fullName.charAt(i));
         }
 
+        //--------------------------------------------------------------------------------------
+
+        // Comparison of 2 strings  
+        String name1 = "Tony" ;
+        String name2 = "Tony" ;
+
+        if(name1.compareTo(name2) > 0 ){
+            System.out.println(name1 + " is bigger than " + name2);
+        }
+        else if(name1.compareTo(name2) == 0){
+            System.out.println("Both the strings are equal");
+        }
+        else{
+            System.out.println(name2 + " is bigger than " + name1);
+        }
+
+        //-----------------------------------------------------------------------------------------
+
+        // substring 
+
+        String sentence = "My name is Tony" ;
+        String name = sentence.substring(11,sentence.length());
+
+        System.out.println(name);
+
+        //------------------------------------------------------------------------------------------
+
+        // Strings are immutable 
         
+
+
 
 
     }
